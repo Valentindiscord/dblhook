c29c39076a26c7b8a1a79d430f507359fc23a9c0
mischievousdev <60783846+mischievousdev@users.noreply.github.com>
2020-06-30 09:21:07 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,27 @@
 import datetime
-import motor.motor_asyncio
-from quart import Quart, request, abort, jsonify
+import pymongo
+from flask import Flask, request, abort, jsonify
 
-app = Quart(__name__)
-db = motor.motor_asyncio.AsyncIOMotorClient("mongodb://pokebot:pokemon@51.79.156.110:27017/?authSource=admin")
+app = Flask(__name__)
+db = pymongo.MongoClient("mongodb://pokebot:pokemon@51.79.156.110:27017/?authSource=admin")
 data = db["pokecord"]
 votes = data["User Upvotes"]
 
 @app.route("/", methods=["POST", "GET"])
-async def index():
+def index():
     return jsonify(dict(request.headers))
 
 @app.route("/webhook", methods=["POST"])
-async def webserver():
+def webserver():
     if request.headers.get("Authorization") == "pokemon":
         user = request.json.get("user")
         bot = request.json.get("bot")
-        checkVoted = await votes.find_one({"UserID": user})
+        checkVoted = votes.find_one({"UserID": user})
         if checkVoted is None:
-            await votes.insert_one({"UserID": user, "Streaks": 1, "Total Votes": 1, "Current Votes Count": 1, "Last Voted At": datetime.datetime.utcnow()})
+            votes.insert_one({"UserID": user, "Streaks": 1, "Total Votes": 1, "Current Votes Count": 1, "Last Voted At": datetime.datetime.utcnow()})
             return "Sucess", 200
         else:
-            await votes.update_one({"UserID": user}, {"$set": {"Streaks": checkVoted["Streaks"] + 1, "Total Votes": checkVoted["Total Votes"] + 1, "Current Votes Count": checkVoted["Current Votes Count"] + 1, "Last Voted At": datetime.datetime.utcnow()}})
+            votes.update_one({"UserID": user}, {"$set": {"Streaks": checkVoted["Streaks"] + 1, "Total Votes": checkVoted["Total Votes"] + 1, "Current Votes Count": checkVoted["Current Votes Count"] + 1, "Last Voted At": datetime.datetime.utcnow()}})
             return "Success", 200
     else:
         abort(400)

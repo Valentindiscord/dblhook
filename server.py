1fc764b92f6e07c30de914beee2f96f8c8f55d93
mischievousdev <60783846+mischievousdev@users.noreply.github.com>
2020-06-30 13:32:28 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import pymongo
 from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
-db = pymongo.MongoClient("mongodb://pokebot:pokemon@51.79.156.110:27017/?authSource=admin")
+db = pymongo.MongoClient("mongodb://pokebot:pokemon@127.0.0.1:27017/?authSource=admin")
 data = db["pokecord"]
 votes = data["User Upvotes"]
 
@@ -17,10 +17,7 @@ def webserver():
         user = request.json.get("user")
         bot = request.json.get("bot")
         checkVoted = votes.find_one({"UserID": user})
-        if checkVoted is None:
-            votes.insert_one({"UserID": user, "Streaks": 1, "Total Votes": 1, "Current Votes Count": 1, "Last Voted At": datetime.datetime.utcnow()})
-            return "Sucess", 200
-        else:
+        if checkVoted is not None:
             votes.update_one({"UserID": user}, {"$set": {"Streaks": checkVoted["Streaks"] + 1, "Total Votes": checkVoted["Total Votes"] + 1, "Current Votes Count": checkVoted["Current Votes Count"] + 1, "Last Voted At": datetime.datetime.utcnow()}})
             return "Success", 200
     else:

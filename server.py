83af41291e2bd0defc7cc0006862b87bdba9f691
mischievousdev <60783846+mischievousdev@users.noreply.github.com>
2020-06-30 06:58:35 +0000

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import flask
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-mongo = pymongo.MongoClient("mongodb://pokebot:pokemon@51.79.156.110:27017/?authSource=admin")
+mongo = pymongo.MongoClient("mongodb://pokebot:pokemon@127.0.0.1:27017/?authSource=admin")
 db = mongo["pokecord"]
 vote = db["User Upvote"]
 
@@ -22,11 +22,13 @@ def whook():
         res = vote.find_one({"UserID": user})
         if res is None:
             vote.insert_one({"UserID": user, "Last Voted At": datetime.datetime.utcnow(), "Streaks": 1})
+            return '', 200
         else:
             total = res["Streaks"] + 1
             vote.update_one({"UserID": user}, {"$set": {"Last Voted At": datetime.datetime.utcnow(), "Streaks": total}})
+            return '', 200
     else:
         return jsonify({"status": "404", "message": "Not authorized"}, 404)
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')    
